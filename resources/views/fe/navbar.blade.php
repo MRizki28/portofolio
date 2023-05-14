9c1cda440663dafa9eef046d1e03bb63dc5f894a
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-14 20:09:18 +0700

    setup ui and create about

---
+++ resources/views/fe/navbar.blade.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Project</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link">Certificate</a>
+              </li>
+             
+              <div class="theme-switch-wrapper">
+                  <label class="theme-switch" for="checkbox">
+                      <input type="checkbox" id="checkbox" />
+                      <div class="slider round"></div>
+                  </label>
+              </div>
+
+          </ul>
+      </div>
+  </div>
+</nav>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+  $(function() {
+      $('#checkbox').change(function() {
+          if ($(this).is(':checked')) {
+              $('body').addClass('dark-mode');
+          } else {
+              $('body').removeClass('dark-mode');
+          }
+      });
+  });
+</script>
---
+++ resources/views/frontend/about.blade.php
@@ -0,0 +1,32 @@
+@extends('fe.base')
+@section('content')
+
+    <div class="container about d-flex justify-content-center">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-12 col-sm-4 text-center pict">
+                <img src="{{ asset('img/rizki.jpeg') }}" alt="avatar-image" class="img-fluid rounded-circle mb-4" style="width: 200px;">
+            </div>
+            <div class="col-12 col-sm-8 text-center text-sm-start mb-5">
+                <h2 class="text-center text-sm-start mb-3 display-4">Muhammad Rizki</h2>
+                <p class="lead mb-3"><i class="fa-solid fa-computer fa-bounce" style="margin-right: 5px"></i> Junior Fullstack Web-Develoepr</p>
+                <p class="lead mb-3"><i class="fa-solid fa-location-crosshairs " style="margin-right: 10px"></i> Palu, Indonesian</p>
+                <div class="mb-3">
+                    <span class="badge  me-2 rounded-0">html</span>
+                    <span class="badge  me-2 rounded-0">css</span>
+                    <span class="badge  me-2 rounded-0">javascript</span>
+                    <span class="badge  me-2 rounded-0">php</span>
+                    <span class="badge  me-2 rounded-0">Laravel</span>
+                
+                </div>
+                <div class="mb-3 medsos ">
+                    <a href="google.com"><i class="fa-brands fa-instagram fa-2x text-secondary" style="margin-right: 5px "></i></a>
+                    <a href="google.com"><i class="fa-brands fa-github fa-2x text-secondary" style="margin-right: 5px "></i></a>
+                    <a href="google.com"><i class="fa-brands fa-linkedin fa-2x text-secondary" style="margin-right: 5px "></i></a>
+                </div>
+                
+               
+            </div>
+        </div>
+    </div>
+    
+@endsection
