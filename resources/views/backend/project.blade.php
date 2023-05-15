9ef9ced2ea5377cd62e427e84b82d1aecb9f26f1
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-15 20:26:09 +0700

    create admin function project add

--- resources/views/backend/project.blade.php
+++ resources/views/backend/project.blade.php
@@ -16,12 +16,13 @@
                                 <tr>
                                     <th>No</th>
                                     <th>Uuid</th>
-                                    <th>Product Name</th>
-                                    <th>Product Model</th>
-                                    <th>Price</th>
-                                    <th>Stock</th>
-                                    <th>Product Image</th>
-                                    <th>Detail ID</th>
+                                    <th>Title</th>
+                                    <th>link</th>
+                                    <th>image</th>
+                                    <th>date</th>
+                                    <th>tecnologi 1</th>
+                                    <th>tecnologi 2</th>
+                                    <th>tecnologi 3</th>
                                     <th>Action</th>
                                 </tr>
                             </thead>
@@ -51,35 +52,41 @@
                         @csrf
                         <input type="hidden" name="uuid">
                         <div class="form-group">
-                            <label for="product_name">Nama Product</label>
-                            <input type="text" class="form-control" name="product_name" id="product_name"
+                            <label for="title">title</label>
+                            <input type="text" class="form-control" name="title" id="title"
                                 placeholder="Input Here..">
                         </div>
                         <div class="form-group">
-                            <label for="product_model">Product Model</label>
-                            <input type="text" class="form-control" name="product_model" id="product_model"
+                            <label for="link">link</label>
+                            <input type="link" class="form-control" name="link" id="link"
                                 placeholder="Input Here">
                         </div>
                         <div class="form-group">
-                            <label for="price">Price</label>
-                            <input type="number" class="form-control" name="price" id="price">
-                        </div>
-                        <div class="form-group">
-                            <label for="stock">Stock</label>
-                            <input type="number" class="form-control" name="stock" id="stock"
+                            <label for="image">Image</label>
+                            <input type="file" class="form-control" name="image" id="image"
                                 placeholder="Input Here">
                         </div>
                         <div class="form-group">
-                            <label for="image_phone">Image</label>
-                            <input type="file" class="form-control" name="image_phone" id="image_phone"
+                            <label for="date">date</label>
+                            <input type="date" class="form-control" name="date" id="date"
                                 placeholder="Input Here">
                         </div>
                         <div class="form-group">
-                            <label for="detail_id">Detail</label>
-                            <select name="detail_id" id="detail_id" class="form-control">
-                                <option value="">-- Pilih Detail --</option>
-                            </select>
+                            <label for="tecnologi1">tecnologi 1</label>
+                            <input type="tecnologi1" class="form-control" name="tecnologi1" id="tecnologi1"
+                                placeholder="Input Here">
                         </div>
+                        <div class="form-group">
+                            <label for="tecnologi2">tecnologi 2</label>
+                            <input type="tecnologi2" class="form-control" name="tecnologi2" id="tecnologi2"
+                                placeholder="Input Here">
+                        </div>
+                        <div class="form-group">
+                            <label for="tecnologi3">tecnologi 3</label>
+                            <input type="tecnologi3" class="form-control" name="tecnologi3" id="tecnologi3"
+                                placeholder="Input Here">
+                        </div>
+                    
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Close</button>
@@ -92,6 +99,77 @@
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script>
+      
+      $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
 
+
+        //create
+
+        $(document).ready(function () { 
+            var formTambah = $('#formTambah');
+
+            formTambah.on('submit' , function (e) { 
+                e.preventDefault();
+                var formData = new FormData(this);
+                $.ajax({
+                    type: 'POST',
+                    url: '{{ url("v1/project/create/") }}',
+                    data: formData,
+                    dataType: 'json' ,
+                    contentType: false,
+                    processData: false,
+                    success: function (data) { 
+                        console.log(data);
+                        if (data.message === 'check your validation') {
+                            var error = data.errors;
+                            var errorMessage = "";
+
+                            $.each(error, function (keu, value) { 
+                                errorMessage += value[0] + "<br>";
+                             });
+
+                             Swal.fire({
+                                tite: 'Error',
+                                html: errorMessage,
+                                icon: 'error',
+                                timer: 5000,
+                                showConfirmButton: true
+                             });
+                        }else {
+                            console.log(data);
+                            Swal.fire({
+                                title: 'Success',
+                                text: 'Data Success Create' ,
+                                icon: 'success',
+                                showCancelButton: false,
+                                confirmButtonText: 'OK'
+                            }).then(function () { 
+                                location.relaod();
+                             });
+                        }
+                     },
+                     error: function (data) { 
+                        var error = data.responseJSON.errors;
+                        var errorMessage = "";
+
+                        $.each(error, function (key, value) { 
+                            errorMessage += value[0] + "<br>";
+                         });
+
+                         Swal.fire({
+                            title: 'Error',
+                            html: errorMessage,
+                            icon: 'error',
+                            timer: 5000,
+                            showConfirmButton: true
+                         });
+                      }
+                });
+             });
+         });
     </script>
 @endsection
