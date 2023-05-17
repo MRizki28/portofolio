16c9c0b3ea398e1dfb2413b7775c88d79e4f1748
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-17 13:49:42 +0700

    create certificate handle ui

---
+++ resources/views/frontend/certificate.blade.php
@@ -0,0 +1,49 @@
+@extends('fe.base')
+@section('content')
+    <div class="container mt-5">
+        <div class="row">
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"
+        integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        $(document).ready(function() {
+            $.ajax({
+                url: "v2/certificate",
+                type: "GET",
+                dataType: "json",
+                success: function(data) {
+                    console.log(data);
+                    // menampilkan semua data
+                    for (var i = 0; i < data.data.length; i++) {
+                        var phone = data.data[i];
+                        phone.price = Number(phone.price).toLocaleString('en');
+                        var card = '<div class="col-md-4 mb-4">' +
+                            '<div class="card">' +
+                            '<h3 class="card-text text-center title" style="margin-bottom:10px;margin-top:10px;">' +
+                            phone.title + '</h3>' +
+                            '<img class="card-img-top" src="/uploads/certificate/' + phone.image +
+                            '" alt="Card image cap" />' +
+                            '<div class="card-body">' +
+                            '<p class="card-text text-center">' + phone.date_publish + ' - ' + phone
+                            .date_expaire + '</p>' +
+                            '<div class="d-flex justify-content-center">' +
+                            '<span class="badge me-2 rounded-0">' + phone.learn1 + '</span>' +
+                            '<span class="badge me-2 rounded-0">' + phone.learn2 + '</span>' +
+                            '<span class="badge me-2 rounded-0">' + phone.learn3 + '</span>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>';
+                        $('.row').append(card);
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.log(xhr.responseText);
+                }
+            });
+        });
+    </script>
+@endsection
